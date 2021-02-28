3fcf7fff1642acf886c84fe928141a215e60a274
akal-bytes <akalerupe@gmail.com>
2021-02-28 15:48:41 +0300

    second assignment

---
+++ src/invokingfunctions.kt
@@ -0,0 +1,32 @@
+fun main() {
+name()
+    var result =modulus(a=5,b=2)
+    println(result)
+    var number=sum(d=5,e=6,f=4,g=7)
+    println(number)
+    fact()
+}
+fun name () {
+val name="sarah"
+    println("hello sarah")
+}
+fun modulus(a: Int,b:Int) :Int {
+
+    var modulus = a % b
+    return modulus
+}
+fun sum(d: Int,e: Int,f: Int,g: Int): Int {
+    var sum=d+e+f+g
+    return sum
+
+}
+fun fact () {
+    var fact="I can use both my left and right hand to carry out tasks"
+    println(fact)
+
+
+}
+
+
+
+
